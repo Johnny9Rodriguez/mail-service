a3b9b5ae65aab27bc53d14859e30060101833b9c
Joe Pytlik <mail@joepytlik.de>
2024-01-10 16:53:29 +0100

    chore: integrate .env into build process, introduce TEMPLATE_PATH for production

--- Services/RouteConfig.cs
+++ Services/RouteConfig.cs
@@ -1,7 +1,18 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 namespace MailService;
 
 public static class RouteConfig
 {
+    static string templatePath;
+
+    static RouteConfig()
+    {
+        DotNetEnv.Env.Load();
+        templatePath = Environment.GetEnvironmentVariable("TEMPLATE_PATH") ?? "./Templates";
+        Console.WriteLine(templatePath);
+    }
+
     public static void ConfigureRoutes(WebApplication app, MailSender mailSender)
     {
         // Default route for testing API communication.
@@ -9,7 +20,8 @@ public static class RouteConfig
 
         app.MapPost("/example", async (ExampleModel model) =>
         {
-            string template = await File.ReadAllTextAsync("./Templates/Example.cshtml");
+            string filePath = Path.Combine(templatePath, "Example.cshtml");
+            string template = await File.ReadAllTextAsync(filePath);
             mailSender.Send(model, template);
         });
     }
