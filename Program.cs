3f27be82ce613d9eeb38df8d098dd726e8380147
Joe Pytlik <mail@joepytlik.de>
2024-01-11 00:47:48 +0100

    feat: enable CORS by allowing localhost as an origin

--- Program.cs
+++ Program.cs
@@ -6,9 +6,28 @@ string apiPort = Environment.GetEnvironmentVariable("API_PORT") ?? "5000";
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls($"http://localhost:{apiPort}");
 builder.Configuration.AddEnvironmentVariables(prefix: "Client_");
+
+// Add localhost and 127.0.0.1 to allowed CORS origins.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        builder =>
+        {
+            builder.AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .SetIsOriginAllowed(origin =>
+                   {
+                       var host = new Uri(origin).Host;
+                       return host == "localhost" || host == "127.0.0.1";
+                   });
+        });
+});
+
 var app = builder.Build();
 
-ClientOptions clientOptions = new (app.Configuration);
+app.UseCors("AllowLocalhost");
+
+ClientOptions clientOptions = new(app.Configuration);
 MailSender mailSender = new(clientOptions);
 
 RouteConfig.ConfigureRoutes(app, mailSender);
