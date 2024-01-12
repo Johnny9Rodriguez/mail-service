1f286635ad1d70853466b543c30264f4ea5ebd26
Joe Pytlik <mail@joepytlik.de>
2024-01-12 02:32:55 +0100

    chore: remove CORS policy from Program.cs

--- Program.cs
+++ Program.cs
@@ -5,25 +5,10 @@ string apiPort = builder.Configuration["Port"] ?? "5000";
 builder.WebHost.UseUrls($"http://localhost:{apiPort}");
 builder.Services.Configure<ClientOptions>(builder.Configuration.GetSection("Client"));
 builder.Services.AddTransient<MailSender>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowLocalhost",
-        builder =>
-        {
-            builder.AllowAnyMethod()
-                   .AllowAnyHeader()
-                   .SetIsOriginAllowed(origin =>
-                   {
-                       var host = new Uri(origin).Host;
-                       return host == "localhost" || host == "127.0.0.1";
-                   });
-        });
-});
 builder.Services.AddHttpLogging(o => { });
 
 var app = builder.Build();
 app.UseHttpLogging();
-app.UseCors("AllowLocalhost");
 
 MailSender mailSender = app.Services.GetRequiredService<MailSender>();
 string templatePath = app.Configuration["TemplatePath"] ?? "./Templates";
